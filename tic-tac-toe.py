4e9c3dfd80469063916be1f54f09f6d241797f0e
Kempth <coolmaxx@hotmail.com>
2022-01-15 11:36:52 -0700

    "Start of Tic-Tac-Toe"

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -4,6 +4,20 @@
 [-, -, -],
 [-, -, -],
 [-, -, -]
-
 ]
+
+user_input between 1-9 according to index above.
+if input isn't in the index ask user to go again
+check if user_input is already used, if so go again
+add input to board
+check for win: rows, columns, and diagonals
+switch between user inputs
+
+
 """
+
+board = [
+    ["-", "-", "-"],
+    ["-", "-", "-"],
+    ["-", "-", "-"]
+]
